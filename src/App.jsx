05213656e53ddb558957ca4dd715122637a71d36
Abdulrahman Okasha <daniue371@gmail.com>
2025-09-21 16:27:16 +0300

    modify routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
-import AboutUs from "./Pages/AboutUs/AboutUs";
-import Contact from "./Pages/Contact/Contact";
+import Routess from "./Routes";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Layout>
-          <Routes>
-            <Route path="/home" element={<Contact />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/services" element={<AboutUs />} />
-            <Route path="/pages" element={<AboutUs />} />
-            <Route path="/blog" element={<AboutUs />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
+          <Routess />
         </Layout>
       </BrowserRouter>
     </>
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,13 +1,24 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import AboutUs from "./Pages/AboutUs/AboutUs";
+import Services from "./Pages/Services/Services";
+import Blog from "./Pages/Blog/Blog";
+import Contact from "./Pages/Contact/Contact";
+import Cart from "./Pages/Pages/Cart/Cart";
 
-function Routes() {
+function Routess() {
   return (
     <>
-      <BrowserRouter>
-        <Routes></Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/pages" element={<Cart />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
 
-export default Routes;
+export default Routess;
