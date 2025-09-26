47954112480f40c42c4ce095ceb36d5a3666888d
Abdulrahman Okasha <daniue371@gmail.com>
2025-09-26 13:55:54 +0300

    separte server and client

--- server/Routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userRoute = express.Router();
-const userController = require("../Controllers/userController");
-
-//------- add --------
-userRoute.post("/addUser", userController.creatUser);
-
-//--------------export user route--------
-module.exports = userRoute;
