c43e7172e2f5c55c8a943ba2da4360272a176fcb
Abdulrahman Okasha <daniue371@gmail.com>
2025-11-02 23:21:55 +0200

    replace button in blog

--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import './BlogCard.css' 
+import Button from "../Button/Button";
+import "./BlogCard.css";
 
-const BlogCard = ({ imgSrc, date, comments, title }) => {  
+const BlogCard = ({ imgSrc, date, comments, title }) => {
   return (
     <>
       <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
         <div className="it-blog-item">
           <div className="it-blog-thumb">
             <a href="#">
-              <img src={imgSrc || "https://ordainit.com/html/educate/assets/img/blog/blog-1-1.jpg"} alt="blog" />  {}
+              <img
+                src={
+                  imgSrc ||
+                  "https://ordainit.com/html/educate/assets/img/blog/blog-1-1.jpg"
+                }
+                alt="blog"
+              />{" "}
+              {}
             </a>
           </div>
           <div className="it-blog-content">
@@ -24,24 +31,25 @@ const BlogCard = ({ imgSrc, date, comments, title }) => {
             </div>
             <h4 className="it-blog-title">
               <a href="#">
-                {title || "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat"}
+                {title ||
+                  "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat"}
               </a>
             </h4>
-            <a className="it-btn" href="#">
-              <span>Read More</span>
-              <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11 1.24023L16 7.24023L11 13.2402" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path>
-                <path d="M1 7.24023H16" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path>
-              </svg>
-            </a>
+            <Button>Read More</Button>
           </div>
         </div>
       </div>
 
       {/* Bootstrap CSS */}
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet" />
+      <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
       {/* FontAwesome */}
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+      />
     </>
   );
 };
