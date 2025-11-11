07793b0582342614d3a395127c3f2747fd5e2d47
Abdulrahman Okasha <daniue371@gmail.com>
2025-11-11 14:34:44 +0200

    modify course details

--- src/Pages/Pages/CourseDetails/CourseDetails.jsx
+++ src/Pages/Pages/CourseDetails/CourseDetails.jsx
@@ -1,23 +1,30 @@
 import "./CourseDetails.css";
-import { FaRegClock, FaRegUser } from "react-icons/fa";
-import { TbFileInvoice } from "react-icons/tb";
-import Button from "./../../../Components/Button/Button";
+import Button from "../../../Components/Button/Button";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import getCourseById from "../../../utilities/GetCourseById";
-import getLessonsByCId from "../../../utilities/getLessonsByCId";
 import VideoPlayer from "../../../Components/VideoPlayer/VideoPlayer";
+import { GoBookmark, GoCommentDiscussion } from "react-icons/go";
+import { LuMenu } from "react-icons/lu";
+import { AiOutlineUser } from "react-icons/ai";
+
+const tabs = [
+  { name: "Overview", icon: GoBookmark },
+  { name: "Curriculum", icon: LuMenu },
+  { name: "Instructor", icon: AiOutlineUser },
+  { name: "Reviews", icon: GoCommentDiscussion },
+];
+
 const CourseDetails = () => {
   const { courseId } = useParams();
   const [course, setCourse] = useState(null);
   const [lessons, setLessons] = useState([]);
   const [activeTab, setActiveTab] = useState("Overview");
-  useEffect(() => {
-    getCourseById(courseId, setCourse);
-    console.log("hiiii");
 
-    getLessonsByCId(courseId, setLessons);
+  useEffect(() => {
+    getCourseById(courseId, setCourse, setLessons);
   }, [courseId]);
+
   if (!course) return <div className="text-center p-5">Loading...</div>;
 
   const videoList = lessons.map((lesson) => ({
@@ -31,161 +38,106 @@ const CourseDetails = () => {
     order: lesson.order,
   }));
 
+  const renderTabContent = {
+    Overview: (
+      <div className="course1-content-roka px-4 py-5 border border-top-0 border-light-subtle">
+        <h5 className="fw-bold">Course Description</h5>
+        <p className="pb-3">{course.shortDescription}</p>
+        <h5 className="fw-bold">What Will I Learn From This Course?</h5>
+        <p>{course.description}</p>
+      </div>
+    ),
+    Curriculum: (
+      <div className="border border-top-0 border-light-subtle">
+        <VideoPlayer videos={videoList} />
+      </div>
+    ),
+    Instructor: <div className="px-4 py-5">Instructor Info Here...</div>,
+    Reviews: <div className="px-4 py-5">Reviews Here...</div>,
+  };
+
   return (
-    //  <h1>{course.title}</h1>
-    <section className="course-details-roka container mt-5 mb-5">
-      <div className="row g-4">
+    <section className="container course-details-roka mt-5 mb-5 m-0 p-0">
+      <div className="row">
         {/* الجزء اليسار */}
-        <div className="col-lg-8">
+        <div className="col-xl-9 col-lg-8">
           <div className="course-image-roka mb-4">
-            {/* <img src="https://ordainit.com/html/educate/assets/img/event/details-1.jpg" className="img-fluid rounded-3" /> */}
-            <img src={course.thumbnailUrl} className="img-fluid rounded-3" />
+            <img src={course.thumbnailUrl} className="rounded-3" />
           </div>
-          <div className="course-rating-roka mb-3">
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-solid fa-star"></i>
-            <i class="fa-regular fa-star"></i>
+
+          <div className="course-rating-roka mb-2">
+            {Array.from({ length: 4 }).map((_, i) => (
+              <i key={i} className="fa-solid fa-star me-1"></i>
+            ))}
+            <i className="fa-regular fa-star me-1"></i>
             <span>(4.5)</span>
           </div>
-          <div className="course-title-roka">
-            <h4>{course.title}</h4>
-          </div>
-          <div className="course-meta-roka d-flex flex-wrap align-items-center gap-4 mt-3 pb-3">
-            <div className="meta-item d-flex align-items-center gap-2">
-              <TbFileInvoice fontSize={"20px"} />
-              <span>Lesson {course.lessonsCount}</span>
-            </div>
-            <div className="meta-item d-flex align-items-center gap-2">
-              <FaRegClock fontSize={"20px"} border />
-              <span>9.00AM-01.00PM</span>
-            </div>
-            <div className="meta-item d-flex align-items-center gap-2">
-              <FaRegUser fontSize={"20px"} />
-              <span>3783 Columbia Mine Road</span>
-            </div>
-          </div>
+
+          <h4 className="course1-title-roka fw-bold mb-4">{course.title}</h4>
 
           {/* Tabs */}
-          {/* <div className="course-tabs-roka mb-4">
-            <button className="active">Overview</button>
-            <button>Curriculum</button>
-            <button>Instructor</button>
-            <button>Reviews</button>
-          </div> */}
-          <ul className="nav nav-tabs">
-            <li className="nav-item">
-              <button
-                className={`nav-link ${
-                  activeTab === "Overview" ? "active" : ""
-                }`}
-                onClick={() => setActiveTab("Overview")}
-              >
-                Overview
-              </button>
-            </li>
-            <li className="nav-item">
-              <button
-                className={`nav-link ${
-                  activeTab === "curriculum" ? "active" : ""
-                }`}
-                onClick={() => setActiveTab("curriculum")}
-              >
-                Curriculum
-              </button>
-            </li>
-            <li className="nav-item">
-              <button
-                className={`nav-link ${
-                  activeTab === "Instructor" ? "active" : ""
-                }`}
-                onClick={() => setActiveTab("Instructor")}
-              >
-                Instructor
-              </button>
-            </li>
-            <li className="nav-item">
-              <button
-                className={`nav-link ${
-                  activeTab === "Reviews" ? "active" : ""
-                }`}
-                onClick={() => setActiveTab("Reviews")}
-              >
-                Reviews
-              </button>
-            </li>
-            {/* ... */}
+          <ul className="nav nav-tabs border-0 gap-2">
+            {tabs.map((tab) => {
+              const Icon = tab.icon;
+              return (
+                <li key={tab.name} className="nav-item">
+                  <button
+                    className="nav-link bg-light"
+                    style={{
+                      color: activeTab === tab.name ? "#0ab99d" : "#181818ff",
+                    }}
+                    onClick={() => setActiveTab(tab.name)}
+                  >
+                    <Icon fontSize="18px" style={{ marginRight: "10px" }} />
+                    {tab.name}
+                  </button>
+                </li>
+              );
+            })}
           </ul>
 
-          {activeTab === "Overview" && (
-            <div className="course-content-roka">
-              <h5>Course Description</h5>
-              {/* <p>
-              Learn JavaScript, HTML, and CSS from scratch. Gain real-world
-              experience through projects and exercises designed to help you
-              master web development step by step. This course is ideal for
-              beginners and intermediate learners.
-            </p> */}
-              <p>{course.shortDescription}</p>
-
-              <h5>What Will I Learn From This Course?</h5>
-              {/* <p>
-              You’ll learn modern JavaScript, responsive design, and advanced
-              HTML/CSS. By the end, you’ll be able to build interactive
-              websites, handle dynamic content, and apply industry-standard
-              practices in web development.
-            </p> */}
-              <p>{course.description}</p>
-            </div>
-          )}
-          {activeTab === "curriculum" && (
-            <div>
-              <VideoPlayer videos={videoList} />
-            </div>
-          )}
-          {activeTab === "instructor" && <div>...</div>}
-          {activeTab === "reviews" && <div>...</div>}
-
-          {/* المحتوى */}
+          {/* Tab Content */}
+          {renderTabContent[activeTab]}
         </div>
 
         {/* الجزء اليمين */}
-        <div className="col-lg-4">
+        <div className="col-xl-3 col-lg-4">
           <div className="course-sidebar-roka shadow-sm p-3 rounded-3">
             <img
               src="https://ordainit.com/html/educate/assets/img/event/details-sm.jpg"
               alt="instructor"
               className="img-fluid rounded mb-3"
             />
-            <div className="course-info-roka">
+            <div className="course-info-roka d-flex justify-content-between">
               <p className="course-label-roka">Course Fee</p>
-              <div className="course-price-roka">
+              <div className="course-price-roka d-flex gap-2">
                 <span className="current-price-roka">${course.price}</span>
                 <span className="old-price-roka">${course.discountPrice}</span>
               </div>
             </div>
-            <p className="guarantee-text-roka">29-Day Money-Back Guarantee</p>
-            <Button>Buy Ticket</Button>
+            <p className="guarantee-text-roka pb-2">
+              29-Day Money-Back Guarantee
+            </p>
+            <Button className="ticketbtn">Buy Ticket</Button>
 
-            <ul className="list-unstyled m-0">
+            <ul className="list-unstyled m-0 mt-3">
               <li>
-                <strong>4:00 pm - 6:00 pm</strong> start date
+                <span>4:00 pm - 6:00 pm</span> <span>start date</span>
               </li>
               <li>
-                <strong>enrolled:</strong> 100
+                <span>enrolled</span> <span>100</span>
               </li>
               <li>
-                <strong>lectures:</strong> 80
+                <span>lectures</span> <span>80</span>
               </li>
               <li>
-                <strong>Skill Level:</strong> Beginner
+                <span>Skill Level</span> <span>Beginner</span>
               </li>
               <li>
-                <strong>Class Day:</strong> Monday - Friday
+                <span>Class Day</span> <span>Monday - Friday</span>
               </li>
               <li>
-                <strong>Language:</strong> English
+                <span>Language</span> <span>English</span>
               </li>
             </ul>
           </div>
