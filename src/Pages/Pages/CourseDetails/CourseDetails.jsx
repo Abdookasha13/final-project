3ad0678ff476836d85d592aefab50b20ec29cec6
Rania <ranyya997@gmail.com>
2025-11-12 02:12:57 +0200

    css changes

--- src/Pages/Pages/CourseDetails/CourseDetails.jsx
+++ src/Pages/Pages/CourseDetails/CourseDetails.jsx
@@ -40,7 +40,7 @@ const CourseDetails = () => {
 
   const renderTabContent = {
     Overview: (
-      <div className="course1-content-roka px-4 py-5 border border-top-0 border-light-subtle">
+      <div className="course1-content-roka px-4 py-4 border border-top-0 border-light-subtle">
         <h5 className="fw-bold">Course Description</h5>
         <p className="pb-3">{course.shortDescription}</p>
         <h5 className="fw-bold">What Will I Learn From This Course?</h5>
@@ -65,7 +65,7 @@ const CourseDetails = () => {
             <img src={course.thumbnailUrl} className="rounded-3" />
           </div>
 
-          <div className="course-rating-roka mb-2">
+          <div className="course-rating-roka mb-2 ">
             {Array.from({ length: 4 }).map((_, i) => (
               <i key={i} className="fa-solid fa-star me-1"></i>
             ))}
@@ -73,18 +73,18 @@ const CourseDetails = () => {
             <span>(4.5)</span>
           </div>
 
-          <h4 className="course1-title-roka fw-bold mb-4">{course.title}</h4>
+          <h4 className="course1-title-roka fw-bold mb-4 ">{course.title}</h4>
 
           {/* Tabs */}
-          <ul className="nav nav-tabs border-0 gap-2">
+          <ul className="nav nav-tabs border-0 d-flex gap-1 flex-nowrap ">
             {tabs.map((tab) => {
               const Icon = tab.icon;
               return (
-                <li key={tab.name} className="nav-item">
+                <li key={tab.name} className="nav-item w-25 fw-bold " >
                   <button
-                    className="nav-link bg-light"
+                    className="nav-link bg-light w-100"
                     style={{
-                      color: activeTab === tab.name ? "#0ab99d" : "#181818ff",
+                      color: activeTab === tab.name ? "#0ab99d" : "#333",
                     }}
                     onClick={() => setActiveTab(tab.name)}
                   >
@@ -102,16 +102,16 @@ const CourseDetails = () => {
 
         {/* الجزء اليمين */}
         <div className="col-xl-3 col-lg-4">
-          <div className="course-sidebar-roka shadow-sm p-3 rounded-3">
+          <div className="course-sidebar-roka shadow-sm p-3 rounded-3 ">
             <img
               src="https://ordainit.com/html/educate/assets/img/event/details-sm.jpg"
               alt="instructor"
               className="img-fluid rounded mb-3"
             />
-            <div className="course-info-roka d-flex justify-content-between">
+            <div className="course-info-roka d-flex justify-content-between ">
               <p className="course-label-roka">Course Fee</p>
               <div className="course-price-roka d-flex gap-2">
-                <span className="current-price-roka">${course.price}</span>
+                <span className="current-price-roka fw-bold">${course.price}</span>
                 <span className="old-price-roka">${course.discountPrice}</span>
               </div>
             </div>
@@ -120,7 +120,7 @@ const CourseDetails = () => {
             </p>
             <Button className="ticketbtn">Buy Ticket</Button>
 
-            <ul className="list-unstyled m-0 mt-3">
+            <ul className="list-unstyled m-0 mt-3 ">
               <li>
                 <span>4:00 pm - 6:00 pm</span> <span>start date</span>
               </li>
