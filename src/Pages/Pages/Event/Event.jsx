4b4037ab44404286087e35b1948d0a9463f35fd8
roqaia <zehakim123456@gmail.com>
2025-12-18 15:58:43 +0200

    modified Event.jsx & utilities/getAllEvents.js and EventCard.css

--- src/Pages/Pages/Event/Event.jsx
+++ src/Pages/Pages/Event/Event.jsx
@@ -1,26 +1,32 @@
 import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 import EventCard from "../../../Components/eventCard/EventCard";
 import "./Event.css";
 import getAllEvents from "../../../utilities/getAllEvents";
 
 function Event() {
   const [events, setEvents] = useState([]);
+  const { i18n } = useTranslation();
 
   useEffect(() => {
-    getAllEvents(setEvents);
+    getAllEvents(setEvents, i18n.language);
     document.title = "Events";
-  }, []);
+  }, [i18n.language]); 
 
   return (
     <div>
-      <div className=" eventcardcontainer ">
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 ">
+      <div className="eventcardcontainer">
+        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+          {events?.length === 0 && (
+            <div className="col-12 text-center py-4">
+              No events available
+            </div>
+          )}
+
           {events?.map((event) => (
             <div className="col" key={event._id}>
-            <EventCard
-              event={event}
-            />
-          </div>
+              <EventCard event={event} />
+            </div>
           ))}
         </div>
       </div>
--- src/utilities/getAllEvents.js
+++ src/utilities/getAllEvents.js
@@ -1,10 +1,30 @@
+// import axios from "axios";
+
+// const getAllEvents = async (setEvents) => {
+//   try {
+//     const res = await axios.get("http://localhost:1911/getAllEvents");
+//     console.log("Fetched Events:", res.data);
+//     setEvents(res.data);
+//   } catch (err) {
+//     console.error("Error fetching events:", err);
+//     setEvents([]);
+//   }
+// };
+
+// export default getAllEvents;
 import axios from "axios";
 
-const getAllEvents = async (setEvents) => {
+const getAllEvents = async (setEvents, lang) => {
   try {
-    const res = await axios.get("http://localhost:1911/getAllEvents");
+    const res = await axios.get(
+      `http://localhost:1911/getAllEvents?lang=${lang}`
+    );
+
     console.log("Fetched Events:", res.data);
     setEvents(res.data);
+
+    // optional cache
+    localStorage.setItem(`events_${lang}`, JSON.stringify(res.data));
   } catch (err) {
     console.error("Error fetching events:", err);
     setEvents([]);
